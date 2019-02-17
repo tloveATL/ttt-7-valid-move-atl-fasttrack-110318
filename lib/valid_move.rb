ee008cc4a6d5eaf92e3e3d2ca8885d211498563e
Tiffani Weaver <tlove.weaver@gmail.com>
2019-02-17 19:25:12 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,19 @@
 # code your #valid_move? method here
-
+def valid_move?(board, index)
+  position = board[index]
+  if position between? (0, 8)
+    return TRUE
+  else
+    return FALSE
+    
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  position = board[index]
+  if position == " " || position == "" || position == nil
+    return false
+else
+    return TRUE
+  end
+end
